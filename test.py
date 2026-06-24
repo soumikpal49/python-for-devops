55b559abeebab3644b1fce91eb6ad39e08be6b87
Soumik Pal <soumikpal49@gmail.com>
2026-06-24 07:43:06 +0530

    Implement most_pop_url function in test.py
    Add a function to find the most popular URL from a log file.

---
+++ test.py
@@ -0,0 +1,18 @@
+from collections import Counter
+from urllib.request import urlopen
+
+def most_pop_url(log_file_url):
+  with urlopen(log_file_url) as response:
+    lines = response.read().decode("utf-8").splitlines()
+
+  url_count = Counter(lines)
+
+most_common_url, count = url_count.most_common_url(1)[0]
+
+print (f"{most_common_url} ({count} occurances)")
+
+logfile1 = "http://public.karat.io/content/url2.txt"
+logfile2 = "http://public.karat.io/content/single_url2.txt"
+
+most_popular_url(logfile1)
+most_popular_url(logfile2)
