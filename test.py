68cff2016b28295f49df8c72af7140d1768166e4
Soumik Pal <soumikpal49@gmail.com>
2026-06-24 07:57:41 +0530

    Rename function to most_popular_url and fix indentation

--- test.py
+++ test.py
@@ -1,15 +1,15 @@
 from collections import Counter
 from urllib.request import urlopen
 
-def most_pop_url(log_file_url):
+def most_popular_url(log_file_url):
   with urlopen(log_file_url) as response:
     lines = response.read().decode("utf-8").splitlines()
 
   url_count = Counter(lines)
 
-most_common_url, count = url_count.most_common_url(1)[0]
+  most_common_url, count = url_count.most_common_url(1)[0]
 
-print (f"{most_common_url} ({count} occurances)")
+  print (f"{most_common_url} ({count} occurances)")
 
 logfile1 = "url2.txt"
 logfile2 = "single_url2.txt"
